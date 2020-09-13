99dfa740fc8ac89c200ce1811acba2de6fab64b9
victorsantanaa <60974316+victorsantanaa@users.noreply.github.com>
2020-09-13 00:34:21 -0300

    Cria novo projeto, APP$ - Alcool ou Gasolina

---
+++ Android/Java/APP 4- Alcool ou Gasolina/APP4 - Alcool ou Gasolina/app/src/main/java/com/jamalicia/alcoolougasolina/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jamalicia.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
