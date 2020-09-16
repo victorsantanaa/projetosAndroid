67a2c1429efccca0dfdd837f0c133d71c330ff35
victorsantanaa <60974316+victorsantanaa@users.noreply.github.com>
2020-09-15 23:22:57 -0300

    commit

--- Android/Java/APP 4- Alcool ou Gasolina/APP4 - Alcool ou Gasolina/app/src/main/java/com/jamalicia/alcoolougasolina/MainActivity.java
+++ Android/Java/APP 4- Alcool ou Gasolina/APP4 - Alcool ou Gasolina/app/src/main/java/com/jamalicia/alcoolougasolina/MainActivity.java
@@ -3,13 +3,65 @@ package com.jamalicia.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editTextAlcool);
+        editPrecoGasolina = findViewById(R.id.editTextGasolina);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularPreco(View view){
+
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+
+        if(camposValidados){
+
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+
+            if(valorAlcool/valorGasolina >= 0.7){
+                textResultado.setText("É melhor utilizar Gasolina!");
+            } else {
+                textResultado.setText("É melhor utilizar Alcool!");
+            }
+
+
+        } else {
+            textResultado.setText("Preencha os preços primeiro!");
+        }
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina) {
+
+        Boolean camposValidados = true;
+        String campo = "preenchido";
+
+        if(pAlcool == null || pAlcool.equals("")){
+            camposValidados = false;
+            campo = "alcool";
+        } else if(pGasolina == null || pGasolina.equals("")) {
+            camposValidados = false;
+            campo = "gasolina";
+        }
+
+
+        return camposValidados;
+
     }
 }
